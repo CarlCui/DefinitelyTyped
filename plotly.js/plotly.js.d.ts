2ef23584a4aeddb968116b558f254f304b6dd2d9
CarlCui <carl.cui@aquaticinformatics.com>
2016-03-28 15:30:44 -0700

    added detailed interfaces (partial)

--- plotly.js/plotly.js.d.ts
+++ plotly.js/plotly.js.d.ts
@@ -1,37 +1,176 @@
 declare namespace Plotly {
+    export interface Projection {
+        rotation?: {lat: number, lon: number, roll: number};
+        scale?: number;
+        type?: string;
+        parallels?: Array<
+    }
+    export interface Geo {
+        domain?: {x: Array<number>, y: Array<number>};
+        projection?: Projection;
+    }
+
+    export interface Camera {
+        eye?: {
+            x: number,
+            y: number,
+            z: number
+        };
+        up?: {
+            x: number,
+            y: number,
+            z: number
+        };
+        center?: {
+            x: number,
+            y: number,
+            z: number
+        };
+    }
+
+    export interface Font {
+        color?: string;
+        family?: string;
+        size?: number;
+    }
+
+    export interface AxisOptions {
+        domain?: Array<number>;
+        showticklabels?: boolean;
+        orientation?: number;
+        ticklen?: number;
+        visible?: boolean;
+        range?: Array<number>;
+        ticksuffix?: string;
+        tickcolor?: string;
+        endpadding?: number;
+        showline?: boolean;
+        tickorientation?: string;
+    }
+
+    export interface Axis {
+        showexponent?: string;
+        showticklabels?: boolean;
+        ticksuffix?: string;
+        showticksuffix?: string;
+        gridwidth?: number;
+        titlefont?: Font;
+        linecolor?: string;
+        mirror?: boolean | string;
+        tickvalssrc?: string;
+        nticks?: number;
+        linewidth?: number;
+        autorange?: boolean | string;
+        ticktextsrc?: string;
+        showspikes?: boolean;
+        tickpredix?: string;
+        tickformat?: string;
+        tickmode?: string;
+        title?: string;
+        spikecolor?: string;
+        ticks?: string;
+        spikesides?: boolean;
+        spikethickness?: number;
+        rangemode?: string;
+        showtickprefix?: string;
+        zeroline?: boolean;
+        gridcolor?: string;
+        type?: string;
+        zerolinewidth?: number;
+        ticklen?: number;
+        hoverformat?: number;
+        backgroundcolor?: string;
+        fixedrange?: boolean;
+        showline?: boolean;
+        ticktext?: Array<string>;
+        showgrid?: boolean;
+        tickvals?: Array<number>;
+        tickfont?: Font;
+        tickwidth?: number;
+        tick0?: number;
+        tickangle?: number;
+        showbackground?: boolean;
+        dtick?: number | string;
+        showaxeslabels?: boolean;
+        zerolinecolor?: string;
+        range?: Array<number>;
+        tickcolor?: string;
+        exponentformat?: string;
+    }
+
+    export interface Scene {
+        domain?: {x: Array<number>, y: Array<number>};
+        dragmode?: string;
+        xaxis?: Axis;
+        yaxis?: Axis;
+        zaxis?: Axis;
+        aspectmode?: string;
+        bgcolor?: string;
+        camera?: Camera;
+        aspectratio?: {
+            x: number,
+            y: number,
+            z: number
+        };
+        hovermode?: string | boolean;
+    }
+
     export interface Trace {
         type: string;
-        x?: any;
-        y?: any;
-        z?: any;
+        x?: Array<number>;
+        y?: Array<number>;
+        z?: Array<number>;
     }
 
     export interface Layout {
         orientation?: number;
         paper_bgcolor?: string;
-        scene?: any;
-        radialaxis?: any;
-        titlefont?: any;
-        hovermode?: any
-        autosize?: any;
+        scene?: Scene;
+        radialaxis?: AxisOptions;
+        titlefont?: Font;
+        hovermode?: string | boolean;
+        autosize?: boolean | string;
         title?: string;
         plot_bgcolor?: string;
         dragmode?: string;
-        xaxis?: any;
-        yaxis?: any;
+        xaxis?: Axis;
+        yaxis?: Axis;
         hidesources?: boolean;
         shapes?: any;
-        annotations?: any;
+        annotations?: {
+            bordercolor?: string,
+            yanchor?: string,
+            text: string,
+            arrowsize?: number,
+            textangle?: number,
+            borderwidth?: number,
+            ay?: number,
+            ax?: number,
+            font?: Font,
+            arrowcolor?: string,
+            xref?: string,
+            arrowhead?: number,
+            bgcolor?: string,
+            borderpad?: number,
+            showarrow?: boolean,
+            opacity?: number,
+            xanchor?: string,
+            arrowwidth?: number,
+            yref?: string,
+            align?: string,
+            y?: number,
+            x?: number
+        };
         direction?: string;
         showlegend?: boolean;
         separators?: string;
-        angularaxis?: any;
+        angularaxis?: AxisOptions;
         geo?: any;
         legend?: any;
         width?: number;
         height?: number;
         margin?: any;
-        font?: any;
+        font?: Font;
     }
 
     export interface Config {
